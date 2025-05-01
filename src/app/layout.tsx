6d1a74981044e8b5df708bb26e5994374f6deda1
ushnishdeybhowmik <neilbhowmik05@gmail.com>
2025-05-01 23:56:01 +0530

    Hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+const analytics = Analytics.toString();
+console.log(analytics);
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
